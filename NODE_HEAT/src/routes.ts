c0516e714a3acb67bcfebddf368a6c57a4551341
fcventura02 <filipecvr4@gmail.com>
2021-10-19 21:09:14 -0300

    Created rout create message

--- NODE_HEAT/src/routes.ts
+++ NODE_HEAT/src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { AuthenticateController } from "./controllers/AuthenticateController";
-import { AuthenticateUserService } from "./services/AuthenticateUserService";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateController().handle);
-
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 export { router };
