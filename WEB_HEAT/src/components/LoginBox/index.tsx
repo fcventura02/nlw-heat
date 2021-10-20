528c3864f5090f319ef1c2b4dd92facca58457b3
fcventura02 <filipecvr4@gmail.com>
2021-10-20 13:32:57 -0300

    Created component loginBox

---
+++ WEB_HEAT/src/components/LoginBox/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.scss";
+import { VscGithubInverted } from "react-icons/vsc";
+export function LoginBox() {
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua menssagem</strong>
+      <a href="#" className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" /> Entrar com Github
+      </a>
+    </div>
+  );
+}
