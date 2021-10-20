39149e2aeb9e6bfa1f85a4b2e74ec75cba3a4c60
fcventura02 <filipecvr4@gmail.com>
2021-10-20 17:09:05 -0300

    connected live messages

--- WEB_HEAT/src/components/MessageList/index.tsx
+++ WEB_HEAT/src/components/MessageList/index.tsx
@@ -1,7 +1,8 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 import { api } from "../../service/api";
 import styles from "./styles.module.scss";
 import logoImg from "../../assets/logo.svg";
-import { useEffect, useState } from "react";
 
 type Message = {
   id: string;
@@ -12,8 +13,27 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
+
   useEffect(() => {
     api
       .get<Message[]>("/messages/last3")
@@ -24,17 +44,19 @@ export function MessageList() {
       <img src={logoImg} alt="" />
 
       <ul className={styles.messageList}>
-        {messages.map((message) => (
-          <li className={styles.message} key={message.id}>
-            <p className={styles.messageContent}>{message.text}</p>
-            <div className={styles.messageUser}>
-              <div className={styles.userImage}>
-                <img src={message.user.avatar_url} alt="" />
+        {messages.map((message) => {
+          return (
+            <li className={styles.message} key={message.id}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt="" />
+                </div>
+                <span>{message.user.name}</span>
               </div>
-              <span>{message.user.name}</span>
-            </div>
-          </li>
-        ))}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
