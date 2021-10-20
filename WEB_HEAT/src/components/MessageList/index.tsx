e5b59c1e96c2c9d7891ae8af0152014a01a8eb21
fcventura02 <filipecvr4@gmail.com>
2021-10-20 14:30:04 -0300

    Connected message list with backend

--- WEB_HEAT/src/components/MessageList/index.tsx
+++ WEB_HEAT/src/components/MessageList/index.tsx
@@ -1,49 +1,40 @@
+import { api } from "../../service/api";
 import styles from "./styles.module.scss";
-
 import logoImg from "../../assets/logo.svg";
+import { useEffect, useState } from "react";
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
 
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  useEffect(() => {
+    api
+      .get<Message[]>("/messages/last3")
+      .then((response) => setMessages(response.data));
+  }, []);
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/fcventura02.png" alt="" />
+        {messages.map((message) => (
+          <li className={styles.message} key={message.id}>
+            <p className={styles.messageContent}>{message.text}</p>
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src={message.user.avatar_url} alt="" />
+              </div>
+              <span>{message.user.name}</span>
             </div>
-            <span>FIlipe Ventura</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/diego3g.png" alt="" />
-            </div>
-            <span>FIlipe Ventura</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-            de todos os tempos, vamooo pra cima! 🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/fcventura02.png" alt="" />
-            </div>
-            <span>FIlipe Ventura</span>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </div>
   );
