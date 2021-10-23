db7db80355e851c631520df49787d4154327e735
fcventura02 <filipecvr4@gmail.com>
2021-10-23 00:53:29 -0300

    Created header

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Header } from "../../components/Header";
 import { styles } from "./styles";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Text</Text>
+      <Header/>
     </View>
   );
 }
